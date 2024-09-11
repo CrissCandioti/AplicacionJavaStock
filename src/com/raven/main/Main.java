e6ed5b8e3bb051ac5b741fa8c52d708ff167322b
CrissCandioti <141766978+CrissCandioti@users.noreply.github.com>
2024-09-11 07:11:17 -0300

    .
    .

--- src/com/raven/main/Main.java
+++ src/com/raven/main/Main.java
@@ -16,13 +16,10 @@ public class Main extends javax.swing.JFrame {
 
     public Main() {
         initComponents();
-        setBackground(new Color(0, 0, 0, 0));
         init();
     }
 
     private void init() {
-        header.initMoving(this);
-        header.initEvent(this, panelBackground1);
         menu.addEventMenu(new EventMenu() {
             @Override
             public void selectedMenu(int index) {
@@ -45,12 +42,10 @@ public class Main extends javax.swing.JFrame {
     private void initComponents() {
 
         panelBackground1 = new com.raven.swing.PanelBackground();
-        header = new com.raven.component.Header();
         menu = new com.raven.menu.Menu();
         mainBody = new com.raven.component.MainBody();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
-        setUndecorated(true);
 
         panelBackground1.setBackground(new java.awt.Color(34, 34, 34));
 
@@ -58,7 +53,6 @@ public class Main extends javax.swing.JFrame {
         panelBackground1.setLayout(panelBackground1Layout);
         panelBackground1Layout.setHorizontalGroup(
             panelBackground1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(header, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
             .addGroup(panelBackground1Layout.createSequentialGroup()
                 .addContainerGap()
                 .addComponent(menu, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
@@ -69,10 +63,9 @@ public class Main extends javax.swing.JFrame {
         panelBackground1Layout.setVerticalGroup(
             panelBackground1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(panelBackground1Layout.createSequentialGroup()
-                .addComponent(header, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addContainerGap()
                 .addGroup(panelBackground1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addComponent(menu, javax.swing.GroupLayout.DEFAULT_SIZE, 597, Short.MAX_VALUE)
+                    .addComponent(menu, javax.swing.GroupLayout.DEFAULT_SIZE, 627, Short.MAX_VALUE)
                     .addComponent(mainBody, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
                 .addContainerGap())
         );
@@ -106,7 +99,6 @@ public class Main extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private com.raven.component.Header header;
     private com.raven.component.MainBody mainBody;
     private com.raven.menu.Menu menu;
     private com.raven.swing.PanelBackground panelBackground1;
