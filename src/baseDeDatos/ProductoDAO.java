75a7001aef6b41162c4c4786a4c39cec44ccb555
CrissCandioti <141766978+CrissCandioti@users.noreply.github.com>
2024-09-25 19:49:56 -0300

    .
    .

---
+++ src/baseDeDatos/ProductoDAO.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package baseDeDatos;
+
+import entidades.Productos;
+
+/**
+ *
+ * @author criss
+ */
+public final class ProductoDAO extends DAO<Productos> {
+
+    @Override
+    public void persistirEntidad(Productos object) {
+        super.persistirEntidad(object); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/OverriddenMethodBody
+    }
+
+    @Override
+    public void actualizarEntidad(Productos object) {
+        super.actualizarEntidad(object); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/OverriddenMethodBody
+    }
+
+    @Override
+    public void borrarEntidad(Productos object) {
+        super.borrarEntidad(object); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/OverriddenMethodBody
+    }
+
+}
---
+++ src/services/ProductoServices.java
@@ -0,0 +1,44 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package services;
+
+import baseDeDatos.ProductoDAO;
+import com.raven.other.ModelProfile;
+import entidades.Productos;
+import entidades.Proveedor;
+import java.util.Date;
+
+/**
+ *
+ * @author criss
+ */
+public class ProductoServices {
+
+    public void persistirProducto(String variedad, String nombre, Date fechaIngreso, String marca, String tipoProducto, String contenido, int stock, double precioCosto, double precioventa, double ganancias, double porcentajeGanancias, Proveedor proveedor, String descripcion, ModelProfile ModelProfile) {
+        try {
+            ProductoDAO dao = new ProductoDAO();
+        } catch (Exception e) {
+            System.out.println("Error en el metodo persistirProducto() de la clase ProductoServices");
+        }
+    }
+
+    public void modificarProducto(int id, String variedad, String nombre, Date fechaIngreso, String marca, String tipoProducto, String contenido, int stock, double precioCosto, double precioventa, double ganancias, double porcentajeGanancias, Proveedor proveedor, String descripcion, ModelProfile ModelProfile) {
+        try {
+            ProductoDAO dao = new ProductoDAO();
+        } catch (Exception e) {
+            System.out.println("Error en el metodo modificarProducto() de la clase ProductoServices");
+        }
+    }
+
+    public void eliminarProducto(Productos aux) {
+        try {
+            ProductoDAO dao = new ProductoDAO();
+            dao.borrarEntidad(aux);
+            System.out.println("Se elimino correctamente el producto");
+        } catch (Exception e) {
+            System.out.println("Error en el metodo eliminarProducto() de la clase ProductoServices");
+        }
+    }
+}
