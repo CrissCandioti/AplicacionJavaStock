96b4e213f881444c29b069f0bc62dc9de6d593e3
CrissCandioti <141766978+CrissCandioti@users.noreply.github.com>
2024-10-06 14:07:59 -0300

    .
    .

--- src/aplicacionjavastock/AplicacionJavaStock.java
+++ src/aplicacionjavastock/AplicacionJavaStock.java
@@ -4,6 +4,15 @@
  */
 package aplicacionjavastock;
 
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+
 /**
  *
  * @author criss
@@ -15,6 +24,24 @@ public class AplicacionJavaStock {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        create();
     }
 
+    private static void create() {
+
+        Workbook book = new HSSFWorkbook();
+        Sheet sheet = book.createSheet("hola java");
+
+        try {
+            FileOutputStream fileout = new FileOutputStream("Excel.xls");
+            book.write(fileout);
+            fileout.close();
+
+        } catch (FileNotFoundException ex) {
+            Logger.getLogger(AplicacionJavaStock.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (IOException ex) {
+            Logger.getLogger(AplicacionJavaStock.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+    }
 }
