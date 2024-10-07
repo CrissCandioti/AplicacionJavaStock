f7326772baf24717ecc1ad27668a6d73dbfda64c
CrissCandioti <141766978+CrissCandioti@users.noreply.github.com>
2024-10-07 10:31:58 -0300

    .
    .

--- src/excel/ClientesExcel.java
+++ src/excel/ClientesExcel.java
@@ -15,6 +15,7 @@ import org.apache.poi.ss.usermodel.CellStyle;
 import org.apache.poi.ss.usermodel.ClientAnchor;
 import org.apache.poi.ss.usermodel.CreationHelper;
 import org.apache.poi.ss.usermodel.Drawing;
+import org.apache.poi.ss.usermodel.Font;
 import org.apache.poi.ss.usermodel.HorizontalAlignment;
 import org.apache.poi.ss.usermodel.Picture;
 import org.apache.poi.ss.usermodel.Sheet;
@@ -28,12 +29,11 @@ import org.apache.poi.xssf.usermodel.XSSFWorkbook;
  * @author criss
  */
 public class ClientesExcel {
-
-        public static void main(String[] args) {
+    
+    public static void main(String[] args) {
         // TODO code application logic here
         ClientesExcelReporte();
     }
-    
     
     public static void ClientesExcelReporte() {
 
@@ -41,7 +41,7 @@ public class ClientesExcel {
         Workbook book = new XSSFWorkbook();
         //Creamos la pestaña
         Sheet sheet = book.createSheet("Clientes");
-
+        
         try {
             //Agregamos una imagen
             InputStream is = new FileInputStream("src\\com\\raven\\icon\\Logo Angel 1.png");
@@ -50,8 +50,8 @@ public class ClientesExcel {
             //Traemos el index de la imagen
             int imgIndex = book.addPicture(bytes, Workbook.PICTURE_TYPE_PNG);
             //Cerramos el inputStream
-            is.close(); 
-            
+            is.close();
+
             //Ahora agregamos esta imagen a nuestro archivo
             CreationHelper help = book.getCreationHelper();
             Drawing draw = sheet.createDrawingPatriarch();//Para poder cargar la imagen
@@ -60,21 +60,30 @@ public class ClientesExcel {
             //Le vamos a indicar en donde se posiciona la imagen, en que columna
             anchor.setCol1(0);
             anchor.setRow1(1);
-            
+
             //Para crear la imagen
             Picture pict = draw.createPicture(anchor, imgIndex);
             //Cambiamos el tamaño
             pict.resize(1, 3);//El parametro donde va a empezar; y cuanto va a consumir de espacio
-            
+
             //Contenido de nuestro reporte
             //Estilo de nuestro titulo
             CellStyle tituloEstilo = book.createCellStyle();
             //Seteamos la ubicacion de nuestro titulo para que este centrado tanto verticalmente como horizontalmente
             tituloEstilo.setAlignment(HorizontalAlignment.CENTER);
             tituloEstilo.setVerticalAlignment(VerticalAlignment.CENTER);
-            
+            //Tipo de fuente
+            Font fuenteTitulo = book.createFont();
+            //Que tipo de fuente va hacer
+            fuenteTitulo.setFontName("Arial");
+            //Que sea negrita
+            fuenteTitulo.setBold(true);
+            //Indicamos el tamaño
+            fuenteTitulo.setFontHeightInPoints((short) 14);//Acepta un tipo de dato short
+            //Se lo asignamis al estilo
+            tituloEstilo.setFont(fuenteTitulo);
             //Contenido de nuestro reporte
-            
+
             //Empezamos a generar el reporte.
             FileOutputStream fileOut = new FileOutputStream("ReporteCliente.xlsx");
             book.write(fileOut);
