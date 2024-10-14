16a82e6e1cda64ecd82b370dfef6623ab29fba2a
CrissCandioti <141766978+CrissCandioti@users.noreply.github.com>
2024-10-14 19:45:07 -0300

    .
    .

---
+++ src/com/raven/form/ChatGPT_Form.java
@@ -0,0 +1,41 @@
+package com.raven.form;
+
+import com.raven.component.Form;
+
+public class ChatGPT_Form extends Form {
+
+    public ChatGPT_Form() {
+        initComponents();
+    }
+
+    @SuppressWarnings("unchecked")
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
+
+        jLabelLogo = new javax.swing.JLabel();
+
+        jLabelLogo.setForeground(new java.awt.Color(0, 0, 0));
+        jLabelLogo.setIcon(new javax.swing.ImageIcon(getClass().getResource("/com/raven/icon/inicio.png"))); // NOI18N
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(this);
+        this.setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addContainerGap(100, Short.MAX_VALUE)
+                .addComponent(jLabelLogo)
+                .addContainerGap(100, Short.MAX_VALUE))
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addGap(25, 25, 25)
+                .addComponent(jLabelLogo)
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+        );
+    }// </editor-fold>//GEN-END:initComponents
+
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JLabel jLabelLogo;
+    // End of variables declaration//GEN-END:variables
+}
