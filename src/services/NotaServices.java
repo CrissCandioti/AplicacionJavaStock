29572ac46d86afa4cbc8515930f8166e726e93e1
CrissCandioti <141766978+CrissCandioti@users.noreply.github.com>
2025-01-14 18:31:40 -0300

    .
    ..

---
+++ src/services/NotaServices.java
@@ -0,0 +1,45 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package services;
+
+import entidades.Notas;
+import java.util.List;
+
+/**
+ *
+ * @author criss
+ */
+public class NotaServices {
+
+    public void persistirNota() {
+        try {
+
+        } catch (Exception e) {
+        }
+    }
+
+    public void modificarNota() {
+        try {
+
+        } catch (Exception e) {
+        }
+    }
+
+    public void borrarNota() {
+        try {
+
+        } catch (Exception e) {
+        }
+    }
+
+    public List<Notas> listaNotas() {
+        try {
+
+        } catch (Exception e) {
+        }
+        return null;
+    }
+    
+}
