54f30fe6889e974bb2e440c5db08a63f6d896802
CrissCandioti <141766978+CrissCandioti@users.noreply.github.com>
2025-01-24 17:41:28 -0300

    .
    .

--- src/services/CompraServices.java
+++ src/services/CompraServices.java
@@ -20,7 +20,6 @@ public class CompraServices {
 
     public void persistirEntidad(Date date, Cliente aux, List<Productos> listaProductos, List<Integer> cantidadComprada, String detalles, double total) {
         try {
-            CompraDAO dao = new CompraDAO();
             if (listaProductos.isEmpty()) {
                 MessageAlerts.getInstance().showMessage("Error al realizar la compra", "La lista de productos se encuentra vacia", MessageAlerts.MessageType.ERROR);
                 return;
@@ -28,8 +27,7 @@ public class CompraServices {
             if (detalles.isEmpty()) {
                 detalles = "Ver detalles";
             }
-            Compra index = new Compra(date, aux, listaProductos, cantidadComprada, detalles, total);
-            dao.persistirEntidad(index);
+            new CompraDAO().persistirEntidad(new Compra(date, aux, listaProductos, cantidadComprada, detalles, total));
             MessageAlerts.getInstance().showMessage("Compra exitosa", "La compra fue realizada correctamente a continuacion se imprimira el recibo", MessageAlerts.MessageType.SUCCESS);
             stockProductosRestar(listaProductos);
         } catch (Exception e) {
@@ -39,9 +37,7 @@ public class CompraServices {
 
     public void actualizarEntidad(int id, Date date, Cliente aux, List<Productos> listaProductos, String detalles, double total) {
         try {
-            CompraDAO dao = new CompraDAO();
-            Compra index = new Compra(id, date, aux, listaProductos, detalles, total);
-            dao.actualizarEntidad(index);
+            new CompraDAO().actualizarEntidad(new Compra(id, date, aux, listaProductos, detalles, total));
             System.out.println("Se actualizo con exitos");
         } catch (Exception e) {
             System.out.println("Error en el metodo actualizarEntidad() de la clase CompraServices");
@@ -50,8 +46,7 @@ public class CompraServices {
 
     public void borrarEntidad(Compra aux) {
         try {
-            CompraDAO dao = new CompraDAO();
-            dao.borrarEntidad(aux);
+            new CompraDAO().borrarEntidad(aux);
             System.out.println("Se elimino correctamente la compra");
         } catch (Exception e) {
             System.out.println("Error en el metodo BorrarEntidad de la clase CompraServices");
@@ -60,41 +55,37 @@ public class CompraServices {
 
     public List<Compra> listaCompra() {
         try {
-            CompraDAO dao = new CompraDAO();
-            return dao.listaCompra();
+            return new CompraDAO().listaCompra();
         } catch (Exception e) {
             System.out.println("Error en el metodo listaCompra de la clase ComprServices");
+            return null;
         }
-        return null;
     }
 
     public List<Compra> listaCompraPorCliente(int id) {
         try {
-            CompraDAO dao = new CompraDAO();
-            return dao.listaDeCompraDeXCliente(id);
+            return new CompraDAO().listaDeCompraDeXCliente(id);
         } catch (Exception e) {
             System.out.println("Error en el metodo listaCompraPorCliente de la clase ComprServices");
+            return null;
         }
-        return null;
     }
 
     public Compra compraPorID(int id) {
         try {
-            CompraDAO dao = new CompraDAO();
-            return dao.compraPorID(id);
+            return new CompraDAO().compraPorID(id);
         } catch (Exception e) {
             System.out.println("Error en la compraPorID de la compraServices");
+            return null;
         }
-        return null;
     }
 
     //Metodo la cualme descuente el stock de lo comprado a la base de datos
     public void stockProductosRestar(List<Productos> listaProductos) {
         try {
             ProductoServices ps = new ProductoServices();
-            Productos index = null;
             for (Productos aux : listaProductos) {
-                index = ps.buscarProductoPorID(aux.getId());
+                Productos index = ps.buscarProductoPorID(aux.getId());
                 index.setStock(index.getStock() - aux.getStock());
                 ps.modificarStock(index);
             }
